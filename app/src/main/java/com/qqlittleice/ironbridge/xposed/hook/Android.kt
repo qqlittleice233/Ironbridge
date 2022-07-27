f5fa18b50c231d54cf7f08e3a4114e635be21a47
QQ littleice <1767523953@qq.com>
2022-07-27 17:54:27 +0800

    add submodule XServiceManager

--- app/src/main/java/com/qqlittleice/ironbridge/xposed/hook/Android.kt
+++ app/src/main/java/com/qqlittleice/ironbridge/xposed/hook/Android.kt
@@ -1,7 +1,13 @@
 package com.qqlittleice.ironbridge.xposed.hook
 
+import com.qqlittleice.ironbridge.xposed.utils.LogUtil
+
 object Android {
 
-    fun hook() {}
+    fun hook() {
+        runCatching {
+
+        }.onFailure { LogUtil.xpe(it) }
+    }
 
 }
