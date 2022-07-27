971773c0701f0ec6d835362f4ec8b7bc8c83d6d6
QQ littleice <1767523953@qq.com>
2022-07-27 18:13:17 +0800

    Update Android.kt

--- app/src/main/java/com/qqlittleice/ironbridge/xposed/hook/Android.kt
+++ app/src/main/java/com/qqlittleice/ironbridge/xposed/hook/Android.kt
@@ -1,12 +1,13 @@
 package com.qqlittleice.ironbridge.xposed.hook
 
+import com.kaisar.xservicemanager.XServiceManager
 import com.qqlittleice.ironbridge.xposed.utils.LogUtil
 
 object Android {
 
     fun hook() {
         runCatching {
-
+            XServiceManager.initForSystemServer()
         }.onFailure { LogUtil.xpe(it) }
     }
 
