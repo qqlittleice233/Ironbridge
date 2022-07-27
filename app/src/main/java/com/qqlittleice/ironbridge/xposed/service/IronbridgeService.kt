cecdf5757432487032a988ed29a11e0047cbefdd
QQ littleice <1767523953@qq.com>
2022-07-27 22:59:32 +0800

    Update IronbridgeService.kt

--- app/src/main/java/com/qqlittleice/ironbridge/xposed/service/IronbridgeService.kt
+++ app/src/main/java/com/qqlittleice/ironbridge/xposed/service/IronbridgeService.kt
@@ -2,77 +2,94 @@ package com.qqlittleice.ironbridge.xposed.service
 
 import com.qqlittleice.ironbridge.IBridgeListener
 import com.qqlittleice.ironbridge.IIronbridge
+import com.qqlittleice.ironbridge.xposed.utils.LogUtil
 
 class IronbridgeService: IIronbridge.Stub() {
 
     private val mListeners = arrayListOf<IBridgeListener>()
 
     override fun addListener(iBridgeListener: IBridgeListener) {
-        mListeners.add(iBridgeListener)
+        runCatching {
+            mListeners.add(iBridgeListener)
+        }.onFailure { LogUtil.xpe(it) }
     }
 
     override fun removeListener(iBridgeListener: IBridgeListener?) {
-        mListeners.remove(iBridgeListener)
+        runCatching {
+            mListeners.remove(iBridgeListener)
+        }.onFailure { LogUtil.xpe(it) }
     }
 
     override fun sendString(tag: String, value: String) {
-        mListeners.forEach {
-            if (it.asBinder().isBinderAlive) {
-                it.onReceivedString(tag, value)
-            } else {
-                mListeners.remove(it)
+        runCatching {
+            mListeners.forEach {
+                if (it.asBinder().isBinderAlive) {
+                    it.onReceivedString(tag, value)
+                } else {
+                    mListeners.remove(it)
+                }
             }
-        }
+        }.onFailure { LogUtil.xpe(it) }
     }
 
     override fun sendInt(tag: String, value: Int) {
-        mListeners.forEach {
-            if (it.asBinder().isBinderAlive) {
-                it.onReceivedInt(tag, value)
-            } else {
-                mListeners.remove(it)
+        runCatching {
+            mListeners.forEach {
+                if (it.asBinder().isBinderAlive) {
+                    it.onReceivedInt(tag, value)
+                } else {
+                    mListeners.remove(it)
+                }
             }
-        }
+        }.onFailure { LogUtil.xpe(it) }
     }
 
     override fun sendLong(tag: String, value: Long) {
-        mListeners.forEach {
-            if (it.asBinder().isBinderAlive) {
-                it.onReceivedLong(tag, value)
-            } else {
-                mListeners.remove(it)
+        runCatching {
+            mListeners.forEach {
+                if (it.asBinder().isBinderAlive) {
+                    it.onReceivedLong(tag, value)
+                } else {
+                    mListeners.remove(it)
+                }
             }
-        }
+        }.onFailure { LogUtil.xpe(it) }
     }
 
     override fun sendFloat(tag: String, value: Float) {
-        mListeners.forEach {
-            if (it.asBinder().isBinderAlive) {
-                it.onReceivedFloat(tag, value)
-            } else {
-                mListeners.remove(it)
+        runCatching {
+            mListeners.forEach {
+                if (it.asBinder().isBinderAlive) {
+                    it.onReceivedFloat(tag, value)
+                } else {
+                    mListeners.remove(it)
+                }
             }
-        }
+        }.onFailure { LogUtil.xpe(it) }
     }
 
     override fun sendDouble(tag: String, value: Double) {
-        mListeners.forEach {
-            if (it.asBinder().isBinderAlive) {
-                it.onReceivedDouble(tag, value)
-            } else {
-                mListeners.remove(it)
+        runCatching {
+            mListeners.forEach {
+                if (it.asBinder().isBinderAlive) {
+                    it.onReceivedDouble(tag, value)
+                } else {
+                    mListeners.remove(it)
+                }
             }
-        }
+        }.onFailure { LogUtil.xpe(it) }
     }
 
     override fun sendBoolean(tag: String, value: Boolean) {
-        mListeners.forEach {
-            if (it.asBinder().isBinderAlive) {
-                it.onReceivedBoolean(tag, value)
-            } else {
-                mListeners.remove(it)
+        runCatching {
+            mListeners.forEach {
+                if (it.asBinder().isBinderAlive) {
+                    it.onReceivedBoolean(tag, value)
+                } else {
+                    mListeners.remove(it)
+                }
             }
-        }
+        }.onFailure { LogUtil.xpe(it) }
     }
 
 
